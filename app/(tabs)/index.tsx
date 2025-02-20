f3b665df5a6a96d5df1c91b46c3a42c70e9274ca
ajones2295 <132492414+ajones2295@users.noreply.github.com>
2025-02-20 17:46:06 -0600

    cganges

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">hello world</ThemedText>
+        <ThemedText type="title">hello worldwww</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
